ec1f2b426b070a64b74f3423222378b3bb063040
Filipe Melo <filipe.melo@liferay.com>
2022-05-25 19:42:46 -0300

    feat: remove unnecessary code

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const routes = Router();
-
-routes.get("/interactions", (request, response) =>
-  response.json({ message: "Hello World!" })
-);
-
-export default routes;
--- src/server.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import routes from "./routes.js";
-
-const server = express();
-server.use(express.json());
-server.use(routes);
-
-export default server;
